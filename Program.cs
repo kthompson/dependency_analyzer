ec139a3b4b1e8a8c35f4229e1ff6227c7edf0448
Kevin Thompson <kevin.thompson@schneider-electric.com>
2015-05-04 08:02:18 -0700

    Added some additional information for exporting.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -7,6 +8,7 @@ using System.Windows.Forms;
 using Mono.Cecil;
 using QuickGraph;
 using QuickGraph.Graphviz;
+using QuickGraph.Graphviz.Dot;
 
 namespace DependencyAnalyzer
 {
@@ -19,29 +21,50 @@ namespace DependencyAnalyzer
         /// </summary>
         static void Main()
         {
-            var list = new List<SEdge<AssemblyDefinition>>();
-
             var commandLineArgs = Environment.GetCommandLineArgs();
 
-            if(commandLineArgs.Length == 0)
+            if(commandLineArgs.Length < 2)
             {
                 PrintHelp();
                 return;
             }
 
-            foreach (var asm in commandLineArgs.Select(AssemblyDefinition.ReadAssembly))
-                AddEdges(asm, list);
+            if (commandLineArgs.Contains("-d"))
+                Debugger.Launch();
+
+            var asms = from asmLocation in commandLineArgs.Skip(1) 
+                       where asmLocation != "-d"
+                       select AssemblyDefinition.ReadAssembly(asmLocation);
+
+            foreach (var asm in asms)
+            {
+                AddEdges(asm, 1);
+            }
 
             //var graph = asm.DependencyEdges().ToAdjacencyGraph<AssemblyDefinition, SEdge<AssemblyDefinition>>();
-            var graph = list.ToAdjacencyGraph<AssemblyDefinition, SEdge<AssemblyDefinition>>();
+            var graph = _edges.ToAdjacencyGraph<AssemblyDefinition, SEdge<AssemblyDefinition>>();
 
             var graphviz = new GraphvizAlgorithm<AssemblyDefinition, SEdge<AssemblyDefinition>>(graph);
             graphviz.FormatVertex += (g, args) =>
-                                         {   
-                                             args.VertexFormatter.Label = args.Vertex.Name.Name;
+                                         {
+                                             string name = args.Vertex.Name.Name;
+                                             args.VertexFormatter.Label = name;
+                                             var x = (float)_edgeList[name].Count / _edges.Count * 100;
+                                             //args.VertexFormatter
+                                             args.VertexFormatter.BottomLabel = _edgeList[name].Count + " edges";
                                          };
 
             // render
+
+            foreach (var kp in _edgeList.OrderByDescending(f => f.Value.Count))
+                Console.Error.WriteLine(kp.Key + ": " + kp.Value.Count);
+
+
+            Console.Error.WriteLine("Total Dependencies: " + References.Count);
+            Console.Error.WriteLine("Total Edges: " + _edges.Count);
+
+
+            
             Console.Write(graphviz.Generate());
         }
 
@@ -57,18 +80,47 @@ namespace DependencyAnalyzer
             return Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().Location);
         }
 
-
-        private static void AddEdges(AssemblyDefinition asm, ICollection<SEdge<AssemblyDefinition>> list)
+        private static void AddEdges(AssemblyDefinition asm, int depth)
         {
+            //if(depth > MaxDepth)
+            //    return;
+
             foreach (var dep in asm.Dependencies())
             {
                 var sEdge = new SEdge<AssemblyDefinition>(asm, dep);
-                if (list.Contains(sEdge))
+                if (!AddSingleEdge(sEdge)) 
                     continue;
-                System.Diagnostics.Trace.WriteLine(sEdge.Source.Name.Name + " => " + sEdge.Target.Name.Name);
-                list.Add(sEdge);
-                AddEdges(dep, list);
+                AddEdges(dep, depth + 1);
             }
+        }
+
+        private static bool AddSingleEdge(SEdge<AssemblyDefinition> sEdge)
+        {
+            if (_edges.Contains(sEdge))
+                return false;
+
+            string source = sEdge.Source.Name.Name;
+            string target = sEdge.Target.Name.Name;
+            
+            if(_exclusions.Contains(source) || _exclusions.Contains(target))
+                return false;
+
+            Trace.WriteLine(source + " => " + target);
+            _edges.Add(sEdge);
+
+            EnsureEdgeList(source);
+            EnsureEdgeList(target);
+
+            if (!_edgeList[source].Contains(target))
+                _edgeList[source].Add(target);
+
+            return true;
+        }
+
+        private static void EnsureEdgeList(string source)
+        {
+            if (!_edgeList.ContainsKey(source))
+                _edgeList.Add(source, new List<string>());
         }
 
         public static IEnumerable<AssemblyDefinition> Dependencies(this AssemblyDefinition asm)
@@ -80,6 +132,26 @@ namespace DependencyAnalyzer
         }
 
         private static readonly Dictionary<string, AssemblyDefinition> References = new Dictionary<string, AssemblyDefinition>();
+        private static readonly List<SEdge<AssemblyDefinition>> _edges = new List<SEdge<AssemblyDefinition>>();
+        private static Dictionary<string, List<string>> _edgeList = new Dictionary<string, List<string>>();
+        private static readonly string[] _exclusions = new string[] {"mscorlib", "System", "System.Core", "System.Xml", "System.Drawing", "System.Windows.Forms", "System.Data", 
+                                                                     "WindowsBase", "PresentationCore", "PresentationFramework", "System.ServiceModel", "System.Web",
+
+        //3.0
+        "ComSvcConfig", "Microsoft.Transactions.Bridge", "Microsoft.Transactions.Bridge.Dtc", "PresentationBuildTasks", "PresentationCFFRasterizer", "PresentationCore", "PresentationFramework", "PresentationFramework.Aero", "PresentationFramework.Classic", "PresentationFramework.Luna", "PresentationFramework.Royale", "PresentationUI", "ReachFramework", "SMSvcHost", "ServiceModelReg", "System.IO.Log", "System.IdentityModel", "System.IdentityModel.Selectors", "System.Printing", "System.Runtime.Serialization", "System.ServiceModel", "System.ServiceModel.Install", "System.ServiceModel.WasHosting", "System.Speech", "System.Workflow.Activities", "System.Workflow.ComponentModel", "System.Workflow.Runtime", "UIAutomationClient", "UIAutomationClientsideProviders", "UIAutomationProvider", "UIAutomationTypes", "WindowsBase", "WindowsFormsIntegration", "WsatConfig", "infocard", 
+
+        //3.5 client
+        "Accessibility", "CustomMarshalers", "ISymWrapper", "Microsoft.JScript", "Microsoft.VisualBasic", "Microsoft.VisualC", "Microsoft.Vsa", "mscorlib", "PresentationCore", "PresentationFramework.Aero", "PresentationFramework.Classic",
+"PresentationFramework", "PresentationFramework.Luna", "PresentationFramework.Royale", "ReachFramework", "System.AddIn.Contract", "System.AddIn", "System.Configuration", "System.Configuration.Install", "System.Core", "System.Data.DataSetExtensions", 
+"System.Data", "System.Data.Linq", "System.Data.Services.Client", "System.Data.SqlXml", "System.Deployment", "System.DirectoryServices", "System", "System.Drawing", "System.EnterpriseServices", "System.IdentityModel", "System.Management",
+"System.Messaging", "System.Net", "System.Printing", "System.Runtime.Remoting", "System.Runtime.Serialization", "System.Runtime.Serialization.Formatters.Soap", "System.Security", "System.ServiceModel", "System.ServiceModel.Web",
+"System.ServiceProcess", "System.Transactions", "System.Web.Services", "System.Windows.Forms", "System.Windows.Presentation", "System.Xml", "System.Xml.Linq", "UIAutomationClient", "UIAutomationClientsideProviders", "UIAutomationProvider",
+"UIAutomationTypes", "WindowsBase", "WindowsFormsIntegration",
+
+        //3.5 full
+        "AddInProcess", "AddInProcess32", "AddInUtil", "DataSvcUtil", "EdmGen", "MSBuild", "Microsoft.Build.Conversion.v3.5", "Microsoft.Build.Engine", "Microsoft.Build.Framework", "Microsoft.Build.Tasks.v3.5", "Microsoft.Build.Utilities.v3.5", "Microsoft.Data.Entity.Build.Tasks", "Microsoft.VisualC.STLCLR", "Sentinel.v3.5Client", "System.AddIn", "System.AddIn.Contract", "System.ComponentModel.DataAnnotations", "System.Core", "System.Data.DataSetExtensions", "System.Data.Entity", "System.Data.Entity.Design", "System.Data.Linq", "System.Data.Services", "System.Data.Services.Client", "System.Data.Services.Design", "System.DirectoryServices.AccountManagement", "System.Management.Instrumentation", "System.Net", "System.ServiceModel.Web", "System.Web.Abstractions", "System.Web.DynamicData", "System.Web.DynamicData.Design", "System.Web.Entity", "System.Web.Entity.Design", "System.Web.Extensions", "System.Web.Extensions.Design", "System.Web.Routing", "System.Windows.Presentation", "System.WorkflowServices", "System.Xml.Linq"};
+        
+        private static int MaxDepth = 2;
 
         private static AssemblyDefinition Resolve(AssemblyNameReference reference)
         {
